488515fd2c287db8dac1ed4840382a7c8e047c8a
Jordan Hawkes <sgoingane@gmail.com>
2022-12-05 16:29:18 -0500

    movement matrix calc and heatmap plot

--- plot_data.py
+++ plot_data.py
@@ -37,6 +37,24 @@ def read_csv(path):
 	
 	return categories, counts, data
 
+def get_movement_matrix(pre, post):
+	categories = pre[0]
+	pre_data = pre[2]
+	post_data = post[2]
+
+	movement = np.zeros((len(categories), len(categories)))
+
+	for cc in range(pre_data.shape[0]):
+		for rr in range(pre_data.shape[1]):
+			if pre_data[cc, rr] != '':
+				found = np.where(post_data==pre_data[cc, rr])
+
+				if found:
+					for f in range(len(found[0])):
+						movement[cc, found[0][f]] += 1
+	print(movement)
+	return movement
+
 def plot_pie(pre, post):
 	fig = plt.figure(figsize=(8,3))
 	fig.add_subplot(121)
@@ -47,8 +65,22 @@ def plot_pie(pre, post):
 
 	plt.show()
 
+def plot_heatmap(pre, post):
+	move = get_movement_matrix(pre, post)
+
+	fig = plt.figure(figsize=(5,5))
+	plt.xlabel('To'), plt.ylabel('From')
+	plt.xticks(ticks=np.arange(len(pre[0])), labels=pre[0], rotation=90)
+	plt.yticks(ticks=np.arange(len(pre[0])), labels=pre[0])
+
+	hm = plt.imshow(move, cmap='inferno')
+	plt.colorbar(hm)
+
+	plt.tight_layout(), plt.show()
+
 if __name__ == '__main__':
 	pre_data = read_csv(path_pre_net)
 	post_data = read_csv(path_post_net)
 
-	plot_pie(pre_data, post_data)
+	# plot_pie(pre_data, post_data)
+	plot_heatmap(pre_data, post_data)
